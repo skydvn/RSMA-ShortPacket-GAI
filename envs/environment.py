94a4baad512cad056ca10285e5f7083d437f8a89
Daniel Nguyen <mduongbkhn@gmail.com>
2023-05-13 17:59:36 +0900

    Update environment.py

--- envs/environment.py
+++ envs/environment.py
@@ -7,7 +7,8 @@ import scipy
 from envs.env_utils import *
 from envs.env_agent_utils import *
 
-class DRGO_env(env_utils, env_agent_utils):
+
+class base_env(env_utils, env_agent_utils):
     def __init__(self, args):
         # Network setting
         self.noise = args.noise
@@ -26,7 +27,7 @@ class DRGO_env(env_utils, env_agent_utils):
         self.P_0 = args.power0
         self.Pn = args.powern
         self.eta = 0.7  # de tinh R_u
-        self.sigma = 3.9811*(np.e**(-21+7))                        # -174 dBm/Hz -> W/Hz
+        self.sigma = 3.9811 * (np.e ** (-21 + 7))  # -174 dBm/Hz -> W/Hz
         # Bandwidth
         self.B = args.bandwidth
 
@@ -35,26 +36,6 @@ class DRGO_env(env_utils, env_agent_utils):
         self.BS_y = 0
         self.BS_R_Range = 1
         self.BS_R_min = 0.1
-
-        # Goal-oriented Settings
-        self.acc_threshold = 0.05
-        self.Lipschitz = 0.005
-        self.inf_capacity = 0.9
-        self.pen_coeff = args.pen_coeff
-
-        self.sigma_data = 0.01
-        self.semantic_mode = args.semantic_mode
-
-        """ =============== """
-        """     Actions     """
-        """ =============== """
-        self.o = np.random.randint(0, self.N_User, size=[self.N_User,1])
-        # tau is Sub-carrier-Allocation. It is an array with form of Num_Nodes interger number,
-        # value change from [0:Num_sub-1] (0 means Sub#1)
-        self.tau = np.random.randint(0, self.N_User, size=[self.N_User,1])
-        # eta is AP-Allocation. It is an array with form of Num_Nodes interger number,
-        # value change from [0:Num_APs-1] (0 means Sub#1)
-        self.P_n = np.reshape((np.random.rand(1, self.N_User) * self.P_u_max), (self.N_User,1))
 
         """ ========================================= """
         """ ===== Function-based Initialization ===== """
@@ -66,7 +47,7 @@ class DRGO_env(env_utils, env_agent_utils):
 
         self.ChannelGain = self._ChannelGain_Calculated(self.sigma_data)
         self.commonDataRate = self._calculateDataRate(self.ChannelGain)
-        self.T = 0                                           # initialize rewards)
+        self.T = 0  # initialize rewards)
 
         """ ============================ """
         """     Environment Settings     """
@@ -75,9 +56,7 @@ class DRGO_env(env_utils, env_agent_utils):
         self.observation_space = self._wrapState().squeeze()
         self.action_space = self._wrapAction()
 
-
     def step(self, action, step):
-        self.tau, self.o, self.P_n = self._decomposeAction(action)
         # Environment change
         self.User_trajectory = np.expand_dims(self._trajectory_U_Generator(), axis=0)
         self.U_location = self.User_trajectory + self.U_location
@@ -86,24 +65,25 @@ class DRGO_env(env_utils, env_agent_utils):
         # Re-calculate channel gain
         self.ChannelGain = self._ChannelGain_Calculated(self.sigma_data)
 
-        self.T = self._Time()    # Generate self.T
+        self.T = self._Time()  # Generate self.T
         # Calculate distortion rate
         sigma_data = self.sigma_data
 
         # We assume that it follows CLT
         # This function can be changed in the future
         temp_c = 20
-        sigma_sem = np.exp(temp_c*(1-self.o)**2)
-        sigma_tot_sqr = 1/((1/sigma_sem**2)+(1/sigma_data**2))
+        sigma_sem = np.exp(temp_c * (1 - self.o) ** 2)
+        sigma_tot_sqr = 1 / ((1 / sigma_sem ** 2) + (1 / sigma_data ** 2))
 
         # Goal-oriented penalty
         if self.semantic_mode == "learn":
-            penalty = max(np.sum((self.eta**2 * self.Lipschitz/2 - self.eta)*\
-                      (self.Lipschitz**2) * sigma_tot_sqr - self.acc_threshold), 0)
+            penalty = max(np.sum((self.eta ** 2 * self.Lipschitz / 2 - self.eta) * \
+                                 (self.Lipschitz ** 2) * sigma_tot_sqr - self.acc_threshold), 0)
         else:
-            penalty = max(np.sum((1/math.sqrt(2*math.pi)) * self.inf_capacity * np.exp( -1/(4*(self.B**2)*sigma_tot_sqr) )),0)
+            penalty = max(np.sum(
+                (1 / math.sqrt(2 * math.pi)) * self.inf_capacity * np.exp(-1 / (4 * (self.B ** 2) * sigma_tot_sqr))), 0)
         # print(f"penalty: {penalty}")
-        reward = - self.T - self.pen_coeff*penalty
+        reward = - self.T - self.pen_coeff * penalty
         print(f"step: {step} --> rew: {reward} | T: {self.T}| pena: {penalty}")
         """
         T = 100 
