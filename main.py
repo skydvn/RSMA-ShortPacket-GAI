9c2a1006be65a2f5de044094457d26656c3b3f0b
Daniel Nguyen <mduongbkhn@gmail.com>
2023-05-13 15:28:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from agents.ddpg.agent import *
+from agents.base_agent import *
 from utils.setting_setup import *
 from envs.environment import *
 
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     args = get_arguments()
 
     env = DRGO_env(args)
+    agent = GAI_agent(args)
 
-    agent = DDPGAgent(
-        args,
-        env
-    )
-    agent.train(args)
+    if flag_train == True:
+        agent.train()
+    if flag_eval == True:
+        agent.evaluate()
+    # En
 
