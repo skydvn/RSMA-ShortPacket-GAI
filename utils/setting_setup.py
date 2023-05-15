dec3bc0f1943aedabbed5178b0e70614190d0e6a
Daniel Nguyen <mduongbkhn@gmail.com>
2023-05-15 21:59:08 +0900

    Update setting_setup.py

--- utils/setting_setup.py
+++ utils/setting_setup.py
@@ -96,6 +96,7 @@ def get_arguments():
                         help="max episode")
     parser.add_argument("--max-step", type=int, default=500,
                         help="max number of step per episode")
-
+    parser.add_argument("--trial", type=int, default=1,
+                        help="number of trial (samplings)")
 
     return parser.parse_args()
