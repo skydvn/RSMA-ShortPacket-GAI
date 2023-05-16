aa0165fb9a5abc37b2a49b8a828724961c11466e
Daniel Nguyen <mduongbkhn@gmail.com>
2023-05-16 12:59:48 +0900

    Update environment.py

--- envs/environment.py
+++ envs/environment.py
@@ -10,23 +10,23 @@ from envs.function_utils import *
 
 class base_env(rsma_utils, env_agent_utils):
     def __init__(self, args):
-        super().__init__()
+        super().__init__(args)
         self.max_step = args.max_step
         self.max_episode = args.max_episode
         self.trial = args.trial
         # Base station initialization
         self.BS_x = 0
         self.BS_y = 0
-        self.BS_R_Range = 1
-        self.BS_R_min = 0.1
+        self.BS_R_Range = args.radius_out            # Radius of cell-out
+        self.BS_R_min = args.radius_in              # Radius of cell-in
 
         """ ========================================= """
         """ =============     Network    ============ """
         """ ========================================= """
-        self.antenna_num = args.antenna_num     # Number of antenna setting
         self.user_num = args.user_num           # Number of users
+        self.antenna_num = args.antenna_num     # Number of antenna setting
         self.bandwidth = args.bandwidth         # Bandwidth in (MHz)
-        self.NFdB = args.NFdB                   # From 280 GHz ~ 330 GHz
+        self.NFdB = args.nfdb                   # From 280 GHz ~ 330 GHz
 
         self.PTdB = np.arange(0,20,2.5)
         self.PTdB1 = np.arange(min(self.PTdB),max(self.PTdB),1)
@@ -40,9 +40,6 @@ class base_env(rsma_utils, env_agent_utils):
         self.G_Tx = np.log10(args.tx_db)        # Antenna Gain of Tx
         self.G_Rx = np.log10(args.rx_db)        # Antenna Gain of Rx
 
-        self.R_in = args.radius_in              # Radius of cell-in
-        self.R_out = args.radius_out            # Radius of cell-out
-
         self.beta_c = args.alloc_common         # Power allocation - common packet
         self.beta_k = (1-self.beta_c)\
                       /self.user_num            # Power allocation - private packet
@@ -51,9 +48,9 @@ class base_env(rsma_utils, env_agent_utils):
         self.rate_k = args.rate_private         # Rate private
 
         self.g_c_threshold = \
-            np.pow(2,self.rate_c)               # Threshold common
+            np.power(2,self.rate_c)               # Threshold common
         self.g_k_threshold = \
-            np.pow(2, self.rate_k)              # Threshold private
+            np.power(2, self.rate_k)              # Threshold private
 
         self.psik = args.imperfect_sic          # Imperfect SIC coefficient
 
@@ -75,8 +72,6 @@ class base_env(rsma_utils, env_agent_utils):
         self.distance_CU_BS = self._distance_Calculated(self.U_location, self.BS_location)
 
         self.ChannelGain = self._ChannelGain_Calculated()
-        # LSF * MAP part of channel hk
-        lk = np.exp(-np.mean())
 
         """ ============================ """
         """     Environment Settings     """
@@ -91,6 +86,7 @@ class base_env(rsma_utils, env_agent_utils):
         """     Environment change      """
         self.User_trajectory = np.expand_dims(self._trajectory_U_Generator(), axis=0)
         self.U_location = self.User_trajectory + self.U_location
+        self.distance_CU_BS = self._distance_Calculated(self.U_location, self.BS_location)
 
         idx_ptdb = 0
         bar_gamma = dB2pow(self.PTdB(idx_ptdb))
@@ -98,7 +94,11 @@ class base_env(rsma_utils, env_agent_utils):
         count_sim_k = np.zeros((1,self.user_num))
 
         for user in range(self.user_num):
+            d_SUk = self.distance_CU_BS[user]
             for trial in range(self.trial):
+                # LSF * MAP part of channel hk
+                lk = np.exp(-np.mean(d_SUk*self.kabs))/np.power(d_SUk,2)
+
                 """      Variable Initialization      """
                 self.m_k = 3            # 1->4
                 self.omega_k = 0.5      # 1
@@ -116,15 +116,15 @@ class base_env(rsma_utils, env_agent_utils):
                 # Channel of user k (Trial,Antenna,1)
                 self.G_k = self.G_nagakami[:,user]
                 # Expect to channel norm - common |gk^h*pc|^2
-                self.gkhpc = np.pow(np.abs(T_conjugate(self.G_k)*self.P_c),2)
+                self.gkhpc = np.power(np.abs(T_conjugate(self.G_k)*self.P_c),2)
                 # Expect to channel norm - private |gk^h*pk|^2
-                self.gkhpk = np.pow(np.abs(T_conjugate(self.G_k)*self.P_k),2)
+                self.gkhpk = np.power(np.abs(T_conjugate(self.G_k)*self.P_k),2)
                 # Channel of other user j (L*(K-1))
                 G_j = self.G_nagakami   # temporary channel
                 G_j[:,user] = []           # remove channel of user k
 
                 # Channel interference vector of other user |gj^h*pk|^2
-                g_j = np.pow(np.abs(T_conjugate(G_j)*self.P_k),2)
+                g_j = np.power(np.abs(T_conjugate(G_j)*self.P_k),2)
 
                 # SINR at user k
                 self.gamma_kc = self.beta_c*bar_gamma
