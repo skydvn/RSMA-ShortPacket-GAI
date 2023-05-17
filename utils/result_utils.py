dfb4ece82fbfe34b52506632928def727af959c6
Daniel Nguyen <mduongbkhn@gmail.com>
2023-05-17 12:06:41 +0900

    Update result_utils.py

--- utils/result_utils.py
+++ utils/result_utils.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def save_results(
-                 scores: List[float],
+                 g_losses: List[float],
                  actor_losses: List[float],
                  critic_losses: List[float],
                  reward_list: List[float],
@@ -21,18 +21,31 @@ def save_results(
 
         with h5py.File(file_path, 'w') as hf:
             hf.create_dataset('reward', data=reward_list)
-            hf.create_dataset('scores', data=scores)
+            hf.create_dataset('g_losses', data=g_losses)
             hf.create_dataset('actor_losses', data=actor_losses)
             hf.create_dataset('critic_losses', data=critic_losses)
 
 
-def save_item(self, item_actor, item_critic, item_name):
-    if not os.path.exists(self.save_folder_name):
-        os.makedirs(self.save_folder_name)
-    torch.save(item_actor, os.path.join(self.save_folder_name, "actor-" + item_name + ".pt"))
-    torch.save(item_critic, os.path.join(self.save_folder_name, "critic-" + item_name + ".pt"))
+def save_item(save_folder_name, item_actor, item_critic, item_name):
+    if not os.path.exists(save_folder_name):
+        os.makedirs(save_folder_name)
+    torch.save(item_actor, os.path.join(save_folder_name, "actor-" + item_name + ".pt"))
+    torch.save(item_critic, os.path.join(save_folder_name, "critic-" + item_name + ".pt"))
 
 
-def load_item(self, item_name):
-    return torch.load(os.path.join(self.save_folder_name, "actor-" + item_name + ".pt")), \
-           torch.load(os.path.join(self.save_folder_name, "critic-" + item_name + ".pt"))
+def load_item(save_folder_name, item_name):
+    return torch.load(os.path.join(save_folder_name, "actor-" + item_name + ".pt")), \
+           torch.load(os.path.join(save_folder_name, "critic-" + item_name + ".pt"))
+
+def save_data(save_folder_name, data):
+    # Create an HDF5 file
+    save_name = save_folder_name + f"/data.h5"
+    file = h5py.File(save_name, "w")
+
+    # Create a dataset within the file and write the data
+    with h5py.File(file, 'w') as hf:
+        hf.create_dataset('train', data=data)
+
+    # Close the file
+    file.close()
+
